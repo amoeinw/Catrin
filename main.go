d4c36217a4d9253b1cf38469b775e17a162bfb17
Aleyandro <aleyandro@Aleyandros-MacBook-Pro.local>
2017-02-25 00:56:33 +0330

    init bee api

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	_ "Amoein/Catrin/Catrin/routers"
+
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
